b5b2099b898524dc305ef26b121e03ebc36f87dd
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-05-21 14:28:26 -0300

    feat(game): implement create game endpoint

---
+++ src/game/entities/game.entity.ts
@@ -0,0 +1,6 @@
+export class Game {
+  id: string;
+  nome: string;
+  descricao: string;
+  number: number;
+}
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { CreateGameDto } from "./dto/create-game.dto";
+import { Game } from "./entities/game.entity";
 
 @Injectable()
 export class GameService {
+
+  games: Game[] = [];
+
   findAll() {
     return 'Buscar todos os jogos';
   }
   create(createGameDto: CreateGameDto) {
-    return 'Registrar um jogo na lista' + JSON.stringify(createGameDto);
+    const game:
+     Game = {
+       id: 'random_id',
+       nome: 'random_nome',
+       descricao: 'random_desc',
+       number: 'random_numb',
+       ...createGameDto
+  }
+
+    this.games.push();
+
+    return game;
   }
 
 }
