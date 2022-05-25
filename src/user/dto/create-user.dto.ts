5477befdb54e17024ac5d080f37a055acfe4ec76
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-05-25 17:19:31 -0300

    feat(user): implement dto & entity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,41 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUrl, Matches, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Login do usuário no Xbox Live',
+    example: 'danielmotas',
+  })
+  nickname: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Nome de usuário. Apenas para exibição',
+    example: 'Daniel Mota',
+  })
+  name: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({
+    description: 'Senha do usuário',
+    example: '@abcd123',
+  })
+  password: string;
+  @ApiProperty({
+    description: 'A confirmação da senha deve ser igual a senha',
+    example: 'Abcd@1234',
+  })
+  confirmPassword: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem de perfil do usuário',
+    example: 'https://avatars.githubusercontent.com/u/97922515?v=4',
+  })
+  image: string;
+}
