52f4daa96a54a57d61150d39527da94f125f927d
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-05-25 17:14:08 -0300

    feat(user)generate resource

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,14 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
 
 export class CreateGameDto{
+  @IsString()
   @ApiProperty({
     description: 'Nome do jogo',
     example: 'Forza 7',
   })
   name: string;
+
   @ApiProperty({
     description: 'Descrição do jogo',
     example: 'História, etc.',
   })
   description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Preço do jogo',
+    example: "199.00",
+  })
+  price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do jogo',
+    example: 'https://m.media-amazon.com/images/I/71RTZKIq4bL._AC_SX342_.jpg',
+  })
+
+  image: string;
 }
--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -2,4 +2,8 @@ export class Game {
   id?: string;
   name: string;
   description: string;
+  price: number;
+  image: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
