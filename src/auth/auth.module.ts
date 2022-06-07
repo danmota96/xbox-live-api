c941214f9a845dc1e12f7955b6948ae9df3064a9
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-06-07 19:52:29 -0300

    feat(jwt): implement strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from "./auth.controller";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { JwtStrategy } from "./jwt.strategy";
 
 
 @Module({
@@ -12,10 +13,10 @@ import { PrismaModule } from "src/prisma/prisma.module";
 		PassportModule.register({ defaultStrategy: "jwt" }),
 		JwtModule.register({
 			secret: process.env.JWT_SECRET,
-			signOptions: { expiresIn: "24h" },
+			signOptions: { expiresIn: "1h" },
 		}),
 	],
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //swagger
-
+  
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
