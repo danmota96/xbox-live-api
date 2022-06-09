7ef1d029ea8ebabc591af2545beb16c3be658bfd
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-06-09 14:34:02 -0300

    fix(auth): change expire

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtStrategy } from "./jwt.strategy";
 		PassportModule.register({ defaultStrategy: "jwt" }),
 		JwtModule.register({
 			secret: process.env.JWT_SECRET,
-			signOptions: { expiresIn: "1h" },
+			signOptions: { expiresIn: "24h" },
 		}),
 	],
 	controllers: [AuthController],
