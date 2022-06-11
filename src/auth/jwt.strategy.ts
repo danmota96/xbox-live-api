62a0cda09f825965821838c1dfb7e01787f84b55
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-06-11 16:12:14 -0300

    format

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -21,9 +21,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new UnauthorizedException('Token inválido');
     }
-
     delete user.password;
-
     return user;
   }
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,8 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
-  IsNumber,
-  IsPositive,
   IsString,
   IsUrl,
   Matches,
